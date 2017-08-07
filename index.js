92a54dfdb58e2852c2bc4b7326164c2faea65c63
VadimKomyshenets <komyshenets@mail.ru>
2017-08-07 18:37:40 +0300

    RegExp

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ var MyForm = {
                     }
                     break;
                 case 'phone':
-                    if (!(isValidRegex(data[field], new RegExp(/\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/)) && checkSum(data[field], 30))) {
+                    if (!(isValidRegex(data[field], new RegExp(/\+7\(\d{3}\)\d{3}-\d{2}-\d{2}$/)) && checkSum(data[field], 30))) {
                         addInvalidField(field);
                     }
                     break;
